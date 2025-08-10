1aff159f9e9659d64faa5b7745485e18b998c153
RamiroMoralesdev <rjaviermoralesjm@gmail.com>
2025-08-10 19:23:54 -0300

    Java

---
+++ Programacion 2/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ramam
+ */
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    int a = 5;
+    int b = 2;
+    int resultado = a / b;
+    System.out.println("Resultado: " + resultado);
+        
+    }
+}
+
+  
