8b490b99fd96180680f09fe949ac6ac431f1524c
RamiroMoralesdev <rjaviermoralesjm@gmail.com>
2025-08-18 20:43:22 -0300

    HELLO WORLD WITH R

---
+++ Probabilidad Y Estadistica/hello_world.r
@@ -0,0 +1,11 @@
+x <- "Hola mundo, Este es mi primer script en R"
+
+print(x)
+
+matriz <- matrix(1:9, nrow = 3, ncol = 3)
+
+print(matriz)
+
+SegundoElemento_PrimeraFila <- matriz[1, 2]
+
+print(SegundoElemento_PrimeraFila)
