8fe4a5e5868396bc645fe5b809f01f9b489de9a4
RamiroMoralesdev <rjaviermoralesjm@gmail.com>
2025-08-31 20:02:38 -0300

    Add analizar promedios en r y pdf de tp 1 y 2

--- Probabilidad Y Estadistica/analizar_datos_promedios.r
+++ Probabilidad Y Estadistica/analizar_datos_promedios.r
@@ -6,27 +6,33 @@ if (!require("readxl")) {
   library(readxl)
 }
 
+# Libreria para calcular moda
+if (!require("modeest")) {
+  install.packages("modeest")
+  library(modeest)
+} else {
+  library(modeest)
+}
+
 
 datos <- read_excel(file.choose()) # Usar file.choose() para seleccionar el archivo. Porque no me esta encontrando el archivo con path 
 
 # Función para calcular moda
 moda <- function(x) {
-  ux <- unique(x)
-  ux[which.max(tabulate(match(x, ux)))]
+  mfv(x, na.rm = TRUE) # Usando la función mfv de modeest
 }
-
 
 vars <- c("Tickets_Soporte", "Tiempo_Conexion")
 
 # Calcular estadísticas. Usando for para iterar y funciones reservadas. cat para imprimir en una sola línea
 for (v in vars) {
-  print("\n---", v, "---\n")
+  cat("\n---", v, "---\n")
   x <- datos[[v]]
   
-  print("Media: ", mean(x, na.rm = TRUE), "\n")
-  print("Mediana: ", median(x, na.rm = TRUE), "\n")
-  print("Moda: ", moda(x), "\n")
-  print("Cuartiles:\n")
-  print(quantile(x, probs = c(0.25, 0.5, 0.75), na.rm = TRUE))
+  cat("Media: ", mean(x, na.rm = TRUE), "\n")
+  cat("Mediana: ", median(x, na.rm = TRUE), "\n")
+  cat("Moda: ", moda(x), "\n")
+  cat("Cuartiles:\n")
+  cat(quantile(x, probs = c(0.25, 0.5, 0.75), na.rm = TRUE), "\n")
   cat("Desvío estándar: ", sd(x, na.rm = TRUE), "\n")
 }
