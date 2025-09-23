23387a083f703cdb8d9837b224745a18a24b4c7f
RamiroMoralesdev <rjaviermoralesjm@gmail.com>
2025-09-22 21:16:56 -0300

    add sql db

---
+++ Bases de Datos 1/PRACTICA-SQL/gestionacademica.sql
@@ -0,0 +1,9 @@
+use gestionacademica;
+INSERT INTO alumnos SET id = 102, nombre="Gabriela", apellido="Ruiz", edad = 35,
+id_carrera = 2;
+INSERT INTO alumnos SET id = 103, nombre="Diego", apellido="Lopez", edad = 29, id_carrera = 3;
+INSERT INTO alumnos SET id = 104, nombre="Mariela", apellido="Puertas", edad = 19
+, id_carrera = 1;
+
+SELECT * FROM carreras;
+SELECT * FROM alumnos;
