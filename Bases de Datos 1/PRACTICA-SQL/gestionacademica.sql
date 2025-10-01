537ccb59f083128c7c768614fe74b3af3d7b9ac8
RamiroMoralesdev <rjaviermoralesjm@gmail.com>
2025-10-01 20:30:28 -0300

    Add script SQL practica 1

--- Bases de Datos 1/PRACTICA-SQL/gestionacademica.sql
+++ Bases de Datos 1/PRACTICA-SQL/gestionacademica.sql
@@ -1,9 +1,84 @@
-use gestionacademica;
-INSERT INTO alumnos SET id = 102, nombre="Gabriela", apellido="Ruiz", edad = 35,
-id_carrera = 2;
-INSERT INTO alumnos SET id = 103, nombre="Diego", apellido="Lopez", edad = 29, id_carrera = 3;
-INSERT INTO alumnos SET id = 104, nombre="Mariela", apellido="Puertas", edad = 19
-, id_carrera = 1;
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gestionacademica
+-- ------------------------------------------------------
+-- Server version	8.0.43
 
-SELECT * FROM carreras;
-SELECT * FROM alumnos;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alumnos`
+--
+
+DROP TABLE IF EXISTS `alumnos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alumnos` (
+  `id` int NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `edad` int DEFAULT NULL,
+  `id_carrera` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_alumnos_id_carrera` (`id_carrera`),
+  CONSTRAINT `fk_alumnos_id_carrera` FOREIGN KEY (`id_carrera`) REFERENCES `carreras` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `asignaturas`
+--
+
+DROP TABLE IF EXISTS `asignaturas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `asignaturas` (
+  `id` int NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `creditos` int DEFAULT NULL,
+  `id_carrera` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_asignaturas_id_carrera` (`id_carrera`),
+  CONSTRAINT `fk_asignaturas_id_carrera` FOREIGN KEY (`id_carrera`) REFERENCES `carreras` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `carreras`
+--
+
+DROP TABLE IF EXISTS `carreras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `carreras` (
+  `id` int NOT NULL,
+  `nombre_carrera` varchar(45) DEFAULT NULL,
+  `duracion` tinyint(1) DEFAULT NULL,
+  `departamento` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'gestionacademica'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-10-01 20:13:14
