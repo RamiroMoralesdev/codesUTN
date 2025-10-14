13d212713b01f711ff6490d3c66f789b22498f08
RamiroMoralesdev <rjaviermoralesjm@gmail.com>
2025-10-14 17:11:38 -0300

    Add initial project structure and implement error handling examples
    - Created project properties and configuration files for NetBeans.
    - Added custom exception class `EdadInvalidaException`.
    - Implemented file reading functionality in `LeerArchivo` class.
    - Developed main application logic in `ManejoErrores` class with examples of error handling.
    - Included a sample text file `archivo.txt` for testing file reading.

---
+++ Programacion 2/ManejoErrores/src/manejoerrores/EdadInvalidaException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manejoerrores;
+
+/**
+ *
+ * @author ramam
+ */
+public class EdadInvalidaException extends RuntimeException {
+      public EdadInvalidaException(String mensaje) {
+        super(mensaje);
+    }
+    
+}
---
+++ Programacion 2/ManejoErrores/src/manejoerrores/LeerArchivo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manejoerrores;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author ramam
+ */
+public class LeerArchivo {
+     public void leerArchivo(String ruta) {
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }
+    
+}
